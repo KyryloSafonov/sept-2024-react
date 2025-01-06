97e48423a5ed17ea1780b245c185e7154c19c7c7
KyryloSafonov <kirilsafon@gmail.com>
2025-01-06 20:33:27 +0200

    lesson 1 test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
-import { FC } from 'react';
+import { FC, PropsWithChildren } from 'react';
 
 import 'App.css';
 
+// 1.Подключала стили Tailwind на второй ветке lessons,зашарила проєкт когда практически доделала домашку стоя на ветки homeworks она третья и первая master.
+// Попыталась подключить стили на ветку homeworks,они не подключились.Что мне необходимо сделать чтобы их подключить и как на гитхаб добавить вторую ветку lessons.
+// 2.
+// На какой версии интелидж работает  TS Jsons2ts у меня он установлен,но почему вместо того чтобы сгенирировать разметку выдает какие то ссылки.
+
+// 1. Чи є принципова різниця використовувати саме стрілочні функції для компонентів, а не function declarations
+//
+// 2. Чи є обовʼязковим класти кожен компонент в окрему папку в межах директорії components, якщо там тільки один
+// файлик без ще одного файлу css (наприклад, коли юзаємо tailwind)
+//
+// 3. Яка різниця між export default та іменованим експортом. Не зрозуміло коли що краще використовувати
+
+interface Props extends PropsWithChildren {
+    className?: string;
+}
+
+const TextWrapper: FC<Props> = (props) => {
+    const { className, children } = props;
+
+    return <div>{children}</div>;
+};
+
 const App: FC = () => {
-    return <div>Home</div>;
+    let num = 1;
+    const count = () => num + 1;
+
+    return (
+        <div>
+            <TextWrapper>
+                <div>123</div>
+            </TextWrapper>
+        </div>
+    );
 };
 
 export default App;
